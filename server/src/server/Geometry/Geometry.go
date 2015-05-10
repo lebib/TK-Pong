c03b7e6858776d4751890b82720281f4cccb0023
DocWinter <sauze.thomas@gmail.com>
2015-05-10 18:26:41 +0200

    Play with Trigonometry
    Add some functions about trigonometry. They'll be helpful for working on
    the physic.

--- server/src/server/Geometry/Geometry.go
+++ server/src/server/Geometry/Geometry.go
@@ -1,5 +1,7 @@
 package Geometry
 
+import "math"
+
 type Point struct {
     X, Y float64
 }
@@ -9,21 +11,22 @@ type Rect struct {
     W, H float64
 }
 
-type Celerity struct {
-    Ratio Point
-}
-
 type Vector struct {
     Coords Point
-    Celerity float64
+    Celerity Point
     Direction float64
 }
 
-func SayHello() string{
-    return "Hello world"
+func Round(f float64) float64 {
+    return math.Floor(f + .5)
 }
 
-func CollisionRR(r1 Rect, r2 Rect) bool{
+func RoundPlus(f float64, places int) (float64) {
+    shift := math.Pow(10, float64(places))
+    return Round(f * shift) / shift
+}
+
+func CollisionRR(r1 Rect, r2 Rect) bool {
     if r1.Coords.X + r1.W < r2.Coords.X  || r1.Coords.X > r2.Coords.X + r2.W {
         if r1.Coords.Y + r1.H < r2.Coords.Y || r1.Coords.Y > r2.Coords.Y + r2.H {
             return false;
@@ -32,7 +35,7 @@ func CollisionRR(r1 Rect, r2 Rect) bool{
     return true;
 }
 
-func CollisionPR(p Point, r Rect) bool{
+func CollisionPR(p Point, r Rect) bool {
     if p.X < r.Coords.X || p.X > r.Coords.X + r.W {
         if p.Y < r.Coords.Y || p.Y > r.Coords.Y + r.H {
             return false;
@@ -41,10 +44,22 @@ func CollisionPR(p Point, r Rect) bool{
     return true;
 }
 
+func GetPointFromAngle(angle float64) Point {
+    point := Point{X: math.Cos(angle), Y: math.Sin(angle)}
+    return Point{X: RoundPlus(point.X, 3), Y: RoundPlus(point.Y, 3)}
+}
+
 func ApplyCelerity(vector Vector) Vector{
+    vector.Coords.X = vector.Coords.X + vector.Celerity.X
+    vector.Coords.Y = vector.Coords.Y + vector.Celerity.Y
     return vector
 }
 
-func LineCoefficient(p1 Point, p2 Point) float64{
-    return (p1.Y - p2.Y) / (p1.X - p2.X) 
+func ApplyBounce(vector Vector, point Point) Vector {
+    return vector
 }
+
+func LineCoefficient(p1 Point, p2 Point) float64 {
+    return (p2.Y - p1.Y) / (p2.X - p1.X) 
+}
+
--- server/src/server/main.go
+++ server/src/server/main.go
@@ -2,18 +2,30 @@ package main
 
 import (
     "fmt"
+    "math"
     "server/Geometry"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
-    p1 := Geometry.Point{X:0, Y:0}
-    p2 := Geometry.Point{X:10, Y:10}
+    // p1 := Geometry.Point{X:0, Y:0}
+    // p2 := Geometry.Point{X:10, Y:10}
+    // r1 := Geometry.Rect{Coords:p1, W:5, H:5}
+    // r2 := Geometry.Rect{Coords:p2, W:5, H:5}
+    p3 := Geometry.Point{X:5, Y:5}
+    p4 := Geometry.GetPointFromAngle(-math.Pi / 2)
+    // p5 := Geometry.GetPointFromAngle(math.Pi)
+    // p6 := Geometry.GetPointFromAngle(-math.Pi / 2)
+    // p7 := Geometry.GetPointFromAngle(-math.Pi)
 
-    r1 := Geometry.Rect{Coords:p1, W:5, H:5}
-    r2 := Geometry.Rect{Coords:p2, W:5, H:5}
-
-    fmt.Println(Geometry.SayHello())
-    fmt.Println(Geometry.CollisionRR(r1, r2))
-    fmt.Println(Geometry.LineCoefficient(p1, p2))
+    fmt.Println(p3.X, p3.Y)
+    fmt.Println(p4.X, p4.Y)
+    // fmt.Println(p5.X, p5.Y)
+    // fmt.Println(p6.X, p6.Y)
+    // fmt.Println(p7.X, p7.Y)
+    p8 := Geometry.Point{X:p3.X+p4.X, Y:p3.Y+p4.Y}
+    fmt.Println(p8.X, p8.Y)
+    fmt.Println(Geometry.LineCoefficient(p3, p8))
+    if math.IsInf(Geometry.LineCoefficient(p3, p8), 0) {
+        fmt.Println("Hello, World!")
+    }
 }
